98e89460ce3c5c4d8a112cc3328160a990b40c53
Francisco Giana <gianafrancisco@gmail.com>
2016-12-13 13:47:39 -0300

    disabled groups logs

--- src/main/java/com/fransis/task/AsyncTaskGetFeed.java
+++ src/main/java/com/fransis/task/AsyncTaskGetFeed.java
@@ -40,7 +40,7 @@ public class AsyncTaskGetFeed implements Runnable{
 
     public void run() {
             for(FbGroup fbGroup: watcher.getGroups()) {
-                log.info("Grupo " + fbGroup.getGroupName());
+                //log.info("Grupo " + fbGroup.getGroupName());
                 groupFeeds = facebookClient.fetchObject("/" + fbGroup.getGroupId() + "/feed", JsonObject.class, Parameter.with("fields", "id,message,from,permalink_url"), Parameter.with("limit", 100));
                 JsonArray data = groupFeeds.getJsonArray("data");
                 for (int i = 0; i < data.length(); i++) {

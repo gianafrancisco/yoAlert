a786a19831a78a7c1dc18870cb334948709fcc85
francisco <gianafrancisco@gmail.com>
2017-08-01 20:30:12 -0300

    added docker database and docker-compose configuration

--- init.sh
+++ init.sh
@@ -1,6 +1,23 @@
-#service mysql start && cd /opt && java -Dserver.port=8080 -jar target/yoAlert-0.0.1.jar
+if [ -z ${DB_HOST} ]
+then
+    DB_HOST="alertas.chmathi3vejv.us-west-2.rds.amazonaws.com"
+fi
+
+if [ -z ${DB_NAME} ]
+then
+    DB_HOST="alertas"
+fi
+
+if [ -z ${DB_NAME} ]
+then
+    DB_USERNAME="alertas"
+fi
+
+
 cd /opt && java -Dsendinblue_key=${SENDINBLUE_KEY} \
                 -Dspring.datasource.password=${DB_PASSWORD} \
+                -Dspring.datasource.url=jdbc:mysql://${DB_HOST}:3306/${DB_NAME} \
+                -Dspring.datasource.username=${DB_USERNAME} \
                 -Dserver.port=8080 \
                 -Dleads_token=${LEADS_TOKEN} \
                 -DemailTo=${EMAIL_TO} \

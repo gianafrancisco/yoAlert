c89b19a0b9967b0a186ff8e9467efd3c73ea25f7
fgiana <fgiana@nxkut.com.ar>
2018-06-11 11:12:45 -0300

    modified Scheduled rate to 300000

--- src/main/java/com/fransis/scheduler/ScheduleTask.java
+++ src/main/java/com/fransis/scheduler/ScheduleTask.java
@@ -43,7 +43,7 @@ public class ScheduleTask {
     @Autowired
     private UsernameRepository usernameRepository;
 
-    @Scheduled(fixedRate = 60000)
+    @Scheduled(fixedRate = 300000)
     public void verificar(){
         List<Watcher> watchers = watcherRepository.findAll();
         for(Watcher watcher: watchers){
